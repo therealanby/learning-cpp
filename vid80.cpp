fd21d7ba57faff2cc526eb65e4c52d4a656df276
therealanby <68487862+therealanby@users.noreply.github.com>
2021-01-03 18:02:31 -0800

    Add files via upload

---
+++ vid80.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+template <typename T>
+void swap(T &a, T &b){
+    T temp = a;
+    a = b;
+    b = tmep;
+}
+template <typename T> //you have to define a template before each method that uses it
+void swap(T a[], T b[], int size){
+
+}
+
+int main(){
+
+}
